05b88f8087cc58fb8ec970b0b1c2da084e3686c8
rebs-code <jack.rebussi@gmail.com>
2024-01-29 16:41:44 +0100

    add: style to cards and display movie info in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,25 @@
 </head>
 
 <body>
+    <header class="text-center">
+        <h1>Movie List</h1>
+    </header>
     <div class="container d-flex flex-wrap">
         @foreach ($movies as $movie)
             <div class="card movie-card">
-                <h3>{{ $movie['title'] }}</h3>
+                <div class="card-title p-2">
+                    <h3>{{ $movie['title'] }}</h3>
+                    <h4>{{ $movie['original_title'] }}</h4>
+                </div>
+                <hr>
+                <div class="card-body">
+                    <h5>Nationality: {{ $movie['nationality'] }}</h5>
+                    <h5>Year: {{ substr($movie['date'], 0, 4) }}</h5>
+                    <h5>Vote: {{ $movie['vote'] }}</h5>
+                </div>
+                <div class="card-footer">
+                    <a href="/" class="btn btn-primary w-100">Show</a>
+                </div>
             </div>
         @endforeach
     </div>
