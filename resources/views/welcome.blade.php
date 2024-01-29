a344713387d0bf2eb71a25619c2d2ef0bd746ca8
rebs-code <80966551+rebs-code@users.noreply.github.com>
2024-01-29 14:59:44 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- my css -->
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <h1>hello laravel</h1>
+</body>
+
+</html>
